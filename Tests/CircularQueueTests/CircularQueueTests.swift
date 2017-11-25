8918aed462cff47d6669ae1f0045224d727fe492
Rene Hexel <r.hexel@griffith.edu.au>
2017-11-26 07:40:24 +1000

    Add value test

--- Tests/CircularQueueTests/CircularQueueTests.swift
+++ Tests/CircularQueueTests/CircularQueueTests.swift
@@ -2,15 +2,24 @@ import XCTest
 @testable import CircularQueue
 
 class CircularQueueTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CircularQueue().text, "Hello, World!")
+    func testValues() {
+        let values = [1, 2, 3, 4, 5, 6, 7, 8]
+        let n = values.count
+        let queue = CircularQueue<Int>(capacity: n)
+        values.forEach(queue.enqueue)
+        XCTAssertEqual(queue.count, n)
+        XCTAssertEqual(queue.readIndex, 0)
+        for i in 0..<n {
+            let k = n - i
+            XCTAssertEqual(queue.count, k)
+            let x = queue.dequeue()
+            XCTAssertEqual(x, values[i])
+            XCTAssertEqual(queue.count, k-1)
+        }
     }
 
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testValues", testValues),
     ]
 }
